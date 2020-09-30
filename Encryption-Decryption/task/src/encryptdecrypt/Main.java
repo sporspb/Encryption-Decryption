19376115b3b7106c964c61506f9a322805590a23
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2020-09-30 14:37:19 +0300

    stage 2 complete

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,7 +1,22 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dv ulfmw z givzhfiv!");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        char[] message = s.toCharArray();
+        int key = scanner.nextInt();
+        int startPos = 96;
+
+        for (int i = 0; i < message.length; i++) {
+            char letter = message[i];
+            if (letter >= 97 && letter <= 122) {
+                int pos = (letter + key - startPos) % 26;
+                message[i] = (char) (startPos + pos);
+            }
+        }
+        System.out.println(new String(message));
     }
 }
