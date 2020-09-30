e812deac98efe02eea109eb6c9a6a7a36c9c6b2c
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2020-09-30 16:01:41 +0300

    stage 3 complete

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -5,18 +5,44 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
         String s = scanner.nextLine();
-        char[] message = s.toCharArray();
+        char[] text = s.toCharArray();
         int key = scanner.nextInt();
-        int startPos = 96;
+        int startPos = 32;
+        int endPos = 126;
 
+        if (command.equals("enc")) {
+            System.out.println(doEncrypt(text, key, startPos, endPos));
+        } else if (command.equals("dec")) {
+            System.out.println(doDecrypt(text, key, startPos, endPos));
+        } else {
+            System.out.println("wrong command");
+        }
+    }
+
+    static String doEncrypt(char[] message, int key, int startPos, int endPos) {
         for (int i = 0; i < message.length; i++) {
             char letter = message[i];
-            if (letter >= 97 && letter <= 122) {
-                int pos = (letter + key - startPos) % 26;
+            if (letter >= startPos && letter <= endPos) {
+                int pos = (letter + key - startPos) % (endPos - startPos + 1);
                 message[i] = (char) (startPos + pos);
             }
         }
-        System.out.println(new String(message));
+        return new String(message);
+    }
+
+    static String doDecrypt(char[] message, int key, int startPos, int endPos) {
+        for (int i = 0; i < message.length; i++) {
+            char letter = message[i];
+            if (letter - key < startPos) {
+                int pos = endPos - (letter - startPos - key);
+                message[i] = (char) (startPos + pos);
+            } else {
+                int pos = (letter - key);
+                message[i] = (char) pos;
+            }
+        }
+        return new String(message);
     }
 }
