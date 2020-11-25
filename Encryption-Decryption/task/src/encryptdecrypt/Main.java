988b2cfbc3c1a7fedc861f6537838e87debdfdb5
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2020-11-26 00:20:26 +0300

    Stage 5 is done!

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,24 +1,49 @@
 package encryptdecrypt;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
         String mode = getMode(args);
+        String fileName = getNameOutputFile(args);
         char[] text = getData(args).toCharArray();
         int key = getKey(args);
         int startPos = 32;
         int endPos = 126;
 
         if (mode.equals("enc")) {
-            System.out.println(doEncrypt(text, key, startPos, endPos));
+            try {
+                outputResult(doEncrypt(text, key, startPos, endPos), fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } else if (mode.equals("dec")) {
-            System.out.println(doDecrypt(text, key, startPos, endPos));
+            try {
+                outputResult(doDecrypt(text, key, startPos, endPos), fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } else {
-            System.out.println("wrong mode");
+            try {
+                outputResult("wrong mode", fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
+
+    public static void outputResult(String text, String fileName) throws IOException {
+        if (fileName.isEmpty()) {
+            System.out.println(text);
+        } else writeToFile(text, fileName);
+    }
+
 
     static String doEncrypt(char[] message, int key, int startPos, int endPos) {
         for (int i = 0; i < message.length; i++) {
@@ -60,7 +85,18 @@ public class Main {
         int pos = getPos(array, "-data");
         if (pos >= 0) {
             return array[pos + 1];
-        } else return "";
+        } else {
+            pos = getPos(array, "-in");
+            if (pos >= 0) {
+                String path = array[pos + 1];
+                try {
+                    return readFileAsString(path);
+                } catch (IOException e) {
+                    System.out.println("Cannot read file: " + e.getMessage());
+                }
+            }
+        }
+        return "";
     }
 
     static int getKey(String[] array) {
@@ -68,5 +104,23 @@ public class Main {
         if (pos >= 0) {
             return Integer.parseInt(array[pos + 1]);
         } else return 0;
+    }
+
+    static String getNameOutputFile(String[] array) {
+        int pos = getPos(array, "-out");
+        if (pos >= 0) {
+            return array[pos + 1];
+        } else return "";
+    }
+
+    static String readFileAsString(String fileName) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
+
+    static void writeToFile(String text, String fileName) throws IOException {
+        File file = new File(fileName);
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(text);
+        }
     }
 }
