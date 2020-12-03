c99802b4ca3dc9e2b977d61be9f80d14af867159
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2020-12-03 23:50:21 +0300

    complete hashCode() and equals() module

---
+++ Problems/Sum of the areas/src/Main.java
@@ -0,0 +1,53 @@
+class Sum {
+    public static int sumOfAreas(Shape[] array) {
+        int sum = 0;
+        for (Shape shp : array) {
+            if (shp.getClass() != Shape.class) {
+                if (shp instanceof Square) {
+                    int side = ((Square) shp).getSide();
+                    sum += side * side;
+                } else if (shp instanceof Rectangle) {
+                    sum += ((Rectangle) shp).getHeight() * ((Rectangle) shp).getWidth();
+                }
+            }
+        }
+        return sum;
+    }
+}
+
+//Don't change the code below
+class Shape {
+}
+
+class Square extends Shape {
+    private int side;
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+}
+
+class Rectangle extends Shape {
+    private int width;
+    private int height;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
