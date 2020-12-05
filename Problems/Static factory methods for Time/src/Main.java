b47118851033cd3580d6951801d92f77895412a6
Yuriy Krasnikov <krasnikov188651@yandex.ru>
2020-12-05 20:07:25 +0300

    complete factory method tasks

---
+++ Problems/Static factory methods for Time/src/Main.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+class Time {
+
+    int hour;
+    int minute;
+    int second;
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public static Time noon() {
+        return new Time(12, 0, 0);
+    }
+
+    public static Time midnight() {
+        return new Time(0, 0, 0);
+    }
+
+    public static Time ofSeconds(long seconds) {
+        int hours = Math.toIntExact(seconds / 3600) % 24;
+        long secs = seconds % 3600;
+        int minutes = Math.toIntExact(secs / 60);
+        int sec = Math.toIntExact(secs % 60);
+
+        return new Time(hours, minutes, sec);
+    }
+
+    public static Time of(int hour, int minute, int second) {
+        return 0 <= hour && hour <= 23 &&
+                0 <= minute && minute <= 59 &&
+                0 <= second && second <= 59 ?
+                new Time(hour, minute, second) : null;
+    }
+}
+
+/* Do not change code below */
+public class Main {
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        final String type = scanner.next();
+        Time time = null;
+
+        switch (type) {
+            case "noon":
+                time = Time.noon();
+                break;
+            case "midnight":
+                time = Time.midnight();
+                break;
+            case "hms":
+                int h = scanner.nextInt();
+                int m = scanner.nextInt();
+                int s = scanner.nextInt();
+                time = Time.of(h, m, s);
+                break;
+            case "seconds":
+                time = Time.ofSeconds(scanner.nextInt());
+                break;
+            default:
+                time = null;
+                break;
+        }
+
+        if (time == null) {
+            System.out.println(time);
+        } else {
+            System.out.println(String.format("%s %s %s", time.hour, time.minute, time.second));
+        }
+    }
+}
